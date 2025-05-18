568e0ee6f9d0554ac32f4e337cd88130c985755d
caio2983 <caioluna2002@gmail.com>
2025-05-17 22:59:59 -0300

    feat: update song component

--- spotify-react/src/components/subcomponents/Song.jsx
+++ spotify-react/src/components/subcomponents/Song.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Song({ setIsSongExpanded }) {
   const handleClick = () => {
     setIsSongExpanded(true);
   };
 
+  const [initialTrack, setInitialTrack] = useState(null);
+
+  useEffect(() => {
+    console.log("Requisição para track inicial disparada");
+    axios
+      .get("http://localhost:3000/initial-track")
+      .then((response) => {
+        setInitialTrack(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  console.log(initialTrack);
+
   return (
     <div className="song-container">
-      Música
       <button onClick={handleClick}>Expandir</button>
+      <div className="song-image-container">
+        <img
+          className="song-image"
+          src={initialTrack?.images[0].url}
+          alt="Song thumbnail"
+        />
+      </div>
     </div>
   );
 }
