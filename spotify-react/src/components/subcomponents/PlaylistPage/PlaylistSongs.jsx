a153dc7fce19d80f0d173b82ae5d1894467e8699
caio2983 <caioluna2002@gmail.com>
2025-05-20 00:52:28 -0300

    feat: delete PlaylistSongs component

--- spotify-react/src/components/subcomponents/PlaylistPage/PlaylistSongs.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function PlaylistSongs() {
-  return (
-    <div className="playlist-songs-container">
-      <div className="songs-header"></div>
-    </div>
-  );
-}
