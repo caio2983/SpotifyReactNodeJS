0aa394883dddfecf42a030e7bd959968b539ec68
caio2983 <caioluna2002@gmail.com>
2025-05-21 17:37:45 -0300

    feat: update playlist page and song

--- spotify-react/src/components/pages/PlaylistPage/PlaylistSong.jsx
+++ spotify-react/src/components/pages/PlaylistPage/PlaylistSong.jsx
@@ -1,9 +1,23 @@
 import React, { useEffect } from "react";
+import { format, formatDistanceToNow, differenceInMonths } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function PlaylistSong({ index, image, song }) {
   useEffect(() => {
     console.log(song);
   }, []);
+
+  function formatDate(inputDate) {
+    const date = new Date(inputDate);
+    const now = new Date();
+
+    if (differenceInMonths(now, date) <= 1) {
+      return formatDistanceToNow(date, { addSuffix: true, locale: ptBR });
+    } else {
+      return format(date, "d 'de' MMMM 'de' yyyy", { locale: ptBR });
+    }
+  }
+
   return (
     <div className="song-card">
       <div className="song-index-album">
@@ -13,6 +27,13 @@ export default function PlaylistSong({ index, image, song }) {
         </div>
       </div>
       <span className="song-album">{song.album.name}</span>
+      <span className="song-date">{formatDate(song.album.release_date)}</span>
+      <span className="song-duration">
+        {Math.floor(song.duration_ms / 60000)}:
+        {Math.floor((song.duration_ms % 60000) / 1000)
+          .toString()
+          .padStart(2, "0")}
+      </span>
     </div>
   );
 }
