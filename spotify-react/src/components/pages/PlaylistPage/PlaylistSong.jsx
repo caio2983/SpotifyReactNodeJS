94dbd1156a19fff75318b5940df34b5b61d06c06
caio2983 <caioluna2002@gmail.com>
2025-05-21 20:00:55 -0300

    feat: add song changing with global state

--- spotify-react/src/components/pages/PlaylistPage/PlaylistSong.jsx
+++ spotify-react/src/components/pages/PlaylistPage/PlaylistSong.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import { format, formatDistanceToNow, differenceInMonths } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { useGlobalContext } from "../../../GlobalContext";
 
 export default function PlaylistSong({ index, image, song }) {
   useEffect(() => {
     console.log(song);
   }, []);
+
+  const { songSelected, setSong } = useGlobalContext();
 
   function formatDate(inputDate) {
     const date = new Date(inputDate);
@@ -14,21 +17,28 @@ export default function PlaylistSong({ index, image, song }) {
     if (differenceInMonths(now, date) <= 1) {
       return formatDistanceToNow(date, { addSuffix: true, locale: ptBR });
     } else {
-      return format(date, "d 'de' MMMM 'de' yyyy", { locale: ptBR });
+      return format(date, "d 'de' MMM. 'de' yyyy", { locale: ptBR });
     }
+  }
+
+  function clickSong() {
+    setSong(song);
   }
 
   return (
     <div className="song-card">
       <div className="song-index-album">
-        <span className="song-index-number">
+        <span className="song-index-number" onClick={clickSong}>
           <span className="song-index"> {index + 1}</span>
 
           <span className="song-play-triangle">▶</span>
         </span>
-
         <div className="song-image-wrapper">
           <img src={image}></img>
+        </div>
+        <div className="name-artist-wrapper">
+          <span className="song-name">{song.name}</span>
+          <span className="song-artist">{song.artists[0].name}</span>
         </div>
       </div>
       <span className="song-album">{song.album.name}</span>
