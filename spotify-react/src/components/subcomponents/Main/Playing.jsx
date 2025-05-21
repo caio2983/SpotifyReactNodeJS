4e856a076daccf897be0bef8b61e64208b668446
caio2983 <caioluna2002@gmail.com>
2025-05-21 20:50:22 -0300

    feat: update playing song component

--- spotify-react/src/components/subcomponents/Main/Playing.jsx
+++ spotify-react/src/components/subcomponents/Main/Playing.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useGlobalContext } from "../../../GlobalContext";
 
 export default function Playing() {
+  const { songSelected, setSong } = useGlobalContext();
+
   return (
-    <div className="playing-song">
-      <span>Playing</span>
+    <div className="playing-container">
+      <div className="playing-wrapper">
+        <div className="playing-details">
+          <div className="playing-image">
+            <img src={songSelected?.album?.images[2].url} alt="Image" />
+          </div>
+          <div className="playing-name-artist">
+            <span className="playing-name text-glow">{songSelected?.name}</span>
+            <span className="playing-artist">
+              {songSelected?.artists[0].name}
+            </span>
+          </div>
+        </div>
+
+        <div className="playing-tools-center"></div>
+
+        <div className="playing-tools-right"></div>
+      </div>
     </div>
   );
 }
