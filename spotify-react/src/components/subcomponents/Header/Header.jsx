2d26a0aa169892ba03c758ccc50f4aa0d78c4c9e
caio2983 <caioluna2002@gmail.com>
2025-05-24 18:43:43 -0300

    feat: add search handling to frontend

--- spotify-react/src/components/subcomponents/Header/Header.jsx
+++ spotify-react/src/components/subcomponents/Header/Header.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import debounce from "lodash.debounce";
 import spotifyLogo from "../../../assets/spotifyLogo.png";
 import { Link } from "react-router-dom";
 import HeaderSearchResults from "./HeaderSearchResults";
 
 export default function Header() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const fetchSearchResults = async (query) => {
+    try {
+      const response = await axios.get(`http://localhost:3000/search/${query}`);
+      setSearchResults(response.data);
+
+      console.log("SEARCHHHH", response.data);
+    } catch (error) {
+      console.error("Erro ao buscar resultados:", error);
+    }
+  };
+
+  const debouncedSearch = useCallback(
+    debounce((query) => {
+      if (query.trim() !== "") {
+        fetchSearchResults(query);
+      } else {
+        setSearchResults([]);
+      }
+    }, 400),
+    []
+  );
+
+  useEffect(() => {
+    debouncedSearch(searchTerm);
+  }, [searchTerm, debouncedSearch]);
+
   return (
     <div className="header">
       <Link to="/" className="logo-wrapper">
@@ -14,10 +45,16 @@ export default function Header() {
         <div className="semi-circle semi-circle-1"></div>
 
         <div className="input-and-results">
-          <input type="text" className="search-input"></input>
+          <input
+            type="text"
+            className="search-input"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
 
-          <HeaderSearchResults></HeaderSearchResults>
+          <HeaderSearchResults />
         </div>
+
         <div className="semi-circle semi-circle-2"></div>
       </div>
     </div>
