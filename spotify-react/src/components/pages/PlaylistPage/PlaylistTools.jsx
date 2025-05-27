83ddac95b9931544dc44b774fceb039fa77e2051
caio2983 <caioluna2002@gmail.com>
2025-05-26 23:37:58 -0300

    feat: update playlist page

--- spotify-react/src/components/pages/PlaylistPage/PlaylistTools.jsx
+++ spotify-react/src/components/pages/PlaylistPage/PlaylistTools.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 export default function PlaylistTools() {
   return (
     <div className="playlist-tools-container">
       <div className="tools-wrapper">
         <div className="play-button-green">
-          <FontAwesomeIcon icon={faPlay} style={{ color: "black" }} size="xl" />
+          <span>▶</span>
         </div>
         <span>+ button</span>
         <span>3 points</span>
