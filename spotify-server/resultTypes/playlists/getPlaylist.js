413d46fa556b3fb445e88c706f9d34be15ccac38
caio2983 <caioluna2002@gmail.com>
2025-05-28 18:10:45 -0300

    fix: fix missing tracks in some playlists

--- spotify-server/resultTypes/playlists/getPlaylist.js
+++ spotify-server/resultTypes/playlists/getPlaylist.js
@@ -4,8 +4,14 @@ const { fetchWebApi } = require("../../fetchWebApi/fetchWebApi");
 async function getPlaylist(id) {
   const endpoint = `v1/playlists/${id}`;
   const result = await fetchWebApi(endpoint, "GET");
+
   const owner_id = result.owner.id;
   const resultOwner = await fetchWebApi(`v1/users/${owner_id}`, "GET");
+
+  if (!result.tracks.items) {
+    const result_items = await fetchWebApi(`v1/playlists/${result.id}/tracks`);
+    result.tracks.items = result_items;
+  }
 
   result.owner = resultOwner;
   return result;
