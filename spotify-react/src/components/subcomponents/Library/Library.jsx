c2b14bf0e78745f72d08c64596ce2051ebe1e24f
caio2983 <caioluna2002@gmail.com>
2025-06-05 11:47:04 -0300

    feat: update library component

--- spotify-react/src/components/subcomponents/Library/Library.jsx
+++ spotify-react/src/components/subcomponents/Library/Library.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import LibraryExpanded from "./LibraryExpanded";
+import { faArrowsLeftRightToLine } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ArtistResultCard from "../Header/SearchResultsCards/ArtistResultCard";
+import NonArtistResultCard from "../Header/SearchResultsCards/NonArtistResultCard";
 
 export default function Library({ setIsExpanded }) {
   const handleClick = () => {
@@ -9,8 +13,33 @@ export default function Library({ setIsExpanded }) {
   return (
     <>
       <div className="library-container">
-        Library
-        <button onClick={handleClick}>Expandir</button>
+        <div className="library-container-header">
+          <FontAwesomeIcon
+            icon={faArrowsLeftRightToLine}
+            onClick={handleClick}
+            className="song-expand-button"
+            size="xl"
+          ></FontAwesomeIcon>
+
+          <span className="your-library">Sua Biblioteca</span>
+        </div>
+
+        <div className="library-tools">
+          <div className="library-buttons-wrapper">
+            <button>Playlists</button>
+            <button>Músicas</button>
+            <button>Artistas</button>
+          </div>
+          <div className="library-search-wrapper">
+            <span>Search Bar</span>
+          </div>
+        </div>
+
+        <div className="results-list">
+          <ArtistResultCard></ArtistResultCard>
+
+          <NonArtistResultCard></NonArtistResultCard>
+        </div>
       </div>
     </>
   );
