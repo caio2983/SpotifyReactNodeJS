9a97ae5a467570e72a5dce50f347053677238ee8
caio2983 <caioluna2002@gmail.com>
2025-06-15 19:55:16 -0300

    feat: update header

--- spotify-react/src/components/subcomponents/Header/Header.jsx
+++ spotify-react/src/components/subcomponents/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import axios from "axios";
 import debounce from "lodash.debounce";
 import spotifyLogo from "../../../assets/spotifyLogo.png";
@@ -6,7 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import HeaderRecentSearchResults from "./HeaderRecentSearchResults";
 import { useGlobalContext } from "../../../GlobalContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   const [searchResults, setSearchResults] = useState([]);
@@ -16,6 +16,7 @@ export default function Header() {
     useGlobalContext();
 
   const navigate = useNavigate();
+  const inputRef = useRef(null);
 
   const fetchSearchResults = async (query) => {
     try {
@@ -56,6 +57,12 @@ export default function Header() {
     debouncedSearch(searchTerm);
   }, [searchTerm, debouncedSearch]);
 
+  const handleFocusInput = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div className="header">
       <Link
@@ -63,30 +70,57 @@ export default function Header() {
         className="logo-wrapper"
         onClick={() => {
           setIsSearching(false);
-          searchTerm("");
+          setSearchTerm("");
         }}
       >
         <img src={spotifyLogo} alt="Spotify Logo" />
       </Link>
-
-      <div className="input-wrapper">
-        <div className="semi-circle semi-circle-1">
-          <FontAwesomeIcon icon={faMagnifyingGlass} size="xl"></FontAwesomeIcon>
+      <div className="home-and-input">
+        <Link
+          to="/"
+          onClick={() => {
+            setIsSearching(false);
+            setSearchTerm("");
+          }}
+        >
+          <div className="home-button">
+            <FontAwesomeIcon icon={faHouse} size="xl"></FontAwesomeIcon>
+          </div>
+        </Link>
+        <div className="input-wrapper">
+          <div
+            className="semi-circle semi-circle-1"
+            onClick={handleFocusInput}
+            role="button"
+            tabIndex={0}
+            onKeyDown={(e) => e.key === "Enter" && handleFocusInput()}
+          >
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              size="2xl"
+              className="magnifying-glass"
+            />
+          </div>
+          <div className="input-and-results">
+            <input
+              ref={inputRef}
+              type="text"
+              className="search-input"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            {recentSearches.length > 0 && (
+              <HeaderRecentSearchResults></HeaderRecentSearchResults>
+            )}
+          </div>
+          <div
+            className="semi-circle semi-circle-2"
+            onClick={handleFocusInput}
+            role="button"
+            tabIndex={0}
+            onKeyDown={(e) => e.key === "Enter" && handleFocusInput()}
+          ></div>
         </div>
-
-        <div className="input-and-results">
-          <input
-            type="text"
-            className="search-input"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          {recentSearches.length > 0 && (
-            <HeaderRecentSearchResults></HeaderRecentSearchResults>
-          )}
-        </div>
-
-        <div className="semi-circle semi-circle-2"></div>
       </div>
     </div>
   );
