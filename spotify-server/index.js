ad55d04cdf8df350e795d005ae6c7dead30c09b0
caio2983 <caioluna2002@gmail.com>
2025-06-15 22:51:31 -0300

    fix: increase payload limit

--- spotify-server/index.js
+++ spotify-server/index.js
@@ -20,7 +20,8 @@ const { user_library } = require("./library/library");
 const app = express();
 const PORT = 3000;
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // const token = process.env.SPOTIFY_TOKEN;
 const clientId = process.env.SPOTIFY_CLIENT_ID;
