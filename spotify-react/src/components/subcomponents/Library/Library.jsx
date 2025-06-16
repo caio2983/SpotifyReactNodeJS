14ee79efcb450ab98f96db5ecaedee0755bbb099
caio2983 <caioluna2002@gmail.com>
2025-06-16 01:27:30 -0300

    feat: update library input

--- spotify-react/src/components/subcomponents/Library/Library.jsx
+++ spotify-react/src/components/subcomponents/Library/Library.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import LibraryExpanded from "./LibraryExpanded";
-import { faArrowsLeftRightToLine } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowsLeftRightToLine,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ArtistResultCard from "../Header/SearchResultsCards/ArtistResultCard";
 import NonArtistResultCard from "../Header/SearchResultsCards/NonArtistResultCard";
@@ -13,7 +16,9 @@ export default function Library({ setIsExpanded, currentWidth, data }) {
   const handleClick = () => {
     setIsExpanded(true);
   };
-
+  const inputRef = useRef(null);
+  const [searchWord, setSearchWord] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
   const { libraryReloadSignal } = useGlobalContext();
   const [items, setItems] = useState([]);
   useEffect(() => {
@@ -22,9 +27,11 @@ export default function Library({ setIsExpanded, currentWidth, data }) {
     });
   }, [libraryReloadSignal]);
 
-  function teste() {
-    console.log("ITEMSSSS", items);
-  }
+  const handleFocusInput = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
 
   return (
     <>
@@ -45,12 +52,35 @@ export default function Library({ setIsExpanded, currentWidth, data }) {
 
           <div className="library-tools">
             <div className="library-buttons-wrapper">
-              <button onClick={teste}>Playlists</button>
+              <button>Playlists</button>
               <button>Músicas</button>
               <button>Artistas</button>
             </div>
-            <div className="library-search-wrapper">
-              <span>Search Bar</span>
+
+            <div className="glass-and-input">
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                size="xl"
+                className="library-magnifying-glass"
+                onClick={() => setShowSearch(true)}
+                style={{ cursor: "pointer" }}
+              />
+
+              <div
+                className={` library-input-wrapper animated-search ${
+                  showSearch ? "expanded" : "collapsed"
+                }`}
+              >
+                <div className="input-and-results">
+                  <input
+                    ref={inputRef}
+                    type="text"
+                    className="search-input"
+                    value={searchWord}
+                    onChange={(e) => setSearchWord(e.target.value)}
+                  />
+                </div>
+              </div>
             </div>
           </div>
 
