cfac5615c7c85c1096bcd7515e75ec92b6b20a3b
caio2983 <caioluna2002@gmail.com>
2025-06-15 23:49:12 -0300

    feat: update playlist page

--- spotify-react/src/components/pages/PlaylistPage/PlaylistPage.jsx
+++ spotify-react/src/components/pages/PlaylistPage/PlaylistPage.jsx
@@ -198,22 +198,24 @@ export default function PlaylistPage() {
         )}
       </header>
 
-      <div
-        className="playlist-songs"
-        style={{
-          background:
-            !isLoading && gradientColor
-              ? `linear-gradient(
-                to bottom,
-             ${hexToRgb(gradientColor, 0.3)} 0%,
-                ${hexToRgb(gradientColor, 0.3)} 10%,
-                ${hexToRgb(gradientColor, 0.1)} 20%,
-           transparent 50%
-              )`
-              : "#1d1d1e",
-        }}
-      >
-        <div className="songs-overlay"></div>
+      <div className="playlist-songs">
+        <div
+          className="songs-overlay"
+          style={{
+            background:
+              !isLoading && gradientColor
+                ? `linear-gradient(
+            to bottom,
+            ${hexToRgb(gradientColor, 0.6)} 0%,
+            ${hexToRgb(gradientColor, 0.4)} 25%,
+            ${hexToRgb(gradientColor, 0.25)} 50%,
+            ${hexToRgb(gradientColor, 0.15)} 70%,
+            ${hexToRgb(gradientColor, 0.0)} 85%,
+            transparent 100%
+          )`
+                : "#1d1d1e",
+          }}
+        ></div>
 
         <PlaylistTools
           playSongs={{
