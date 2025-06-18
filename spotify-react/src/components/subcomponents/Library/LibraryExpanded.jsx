1e246ff834da4e764e387b01e1083fa857d731d5
caio2983 <caioluna2002@gmail.com>
2025-06-18 20:34:42 -0300

    feat: wrap library item details in Link components for navigation

--- spotify-react/src/components/subcomponents/Library/LibraryExpanded.jsx
+++ spotify-react/src/components/subcomponents/Library/LibraryExpanded.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useGlobalContext } from "../../../GlobalContext";
-
+import { Link } from "react-router-dom";
 export default function LibraryExpanded({ setIsExpanded }) {
   const [animateClass, setAnimateClass] = useState("collapsed");
   const { libraryItems } = useGlobalContext();
@@ -38,38 +38,40 @@ export default function LibraryExpanded({ setIsExpanded }) {
             }
           >
             <div className="library-expanded-item-content">
-              <img
-                alt={item.name}
-                className={
-                  item.type === "artist"
-                    ? "library-expanded-item-image-artist"
-                    : "library-expanded-item-image"
-                }
-                src={
-                  item.type === "artist"
-                    ? item?.images?.[1]?.url
-                    : item?.album?.images?.[1]?.url ||
-                      item?.images?.[1]?.url ||
-                      item?.album?.images[0].url ||
-                      item?.images?.[0].url
-                }
-              />
-              <div className="library-expanded-item-info">
-                <span className="slide-name library-expanded-item-title ">
-                  {item.name}
-                </span>
-                <span className="library-expanded-item-info slide-artist">
-                  {item.type === "artist"
-                    ? item?.artists?.[0]?.name ??
-                      item?.owner?.display_name ??
-                      "Artista"
-                    : item.type === "album"
-                    ? item?.artists?.[0]?.name ?? "Álbum"
-                    : item?.album?.artists?.[0]?.name ??
-                      item?.owner?.display_name ??
-                      "Desconhecido"}
-                </span>
-              </div>
+              <Link to={`/${item.type}/${item.id}`}>
+                <img
+                  alt={item.name}
+                  className={
+                    item.type === "artist"
+                      ? "library-expanded-item-image-artist"
+                      : "library-expanded-item-image"
+                  }
+                  src={
+                    item.type === "artist"
+                      ? item?.images?.[1]?.url
+                      : item?.album?.images?.[1]?.url ||
+                        item?.images?.[1]?.url ||
+                        item?.album?.images[0].url ||
+                        item?.images?.[0].url
+                  }
+                />
+                <div className="library-expanded-item-info">
+                  <span className="slide-name library-expanded-item-title ">
+                    <Link to={`/${item.type}/${item.id}`}>{item.name}</Link>
+                  </span>
+                  <span className="library-expanded-item-info slide-artist">
+                    {item.type === "artist"
+                      ? item?.artists?.[0]?.name ??
+                        item?.owner?.display_name ??
+                        "Artista"
+                      : item.type === "album"
+                      ? item?.artists?.[0]?.name ?? "Álbum"
+                      : item?.album?.artists?.[0]?.name ??
+                        item?.owner?.display_name ??
+                        "Desconhecido"}
+                  </span>
+                </div>
+              </Link>
             </div>
           </div>
         ))}
