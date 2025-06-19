a88b2058017e21f6bd0c29afff3901c110f3055a
caio2983 <caioluna2002@gmail.com>
2025-06-18 23:18:52 -0300

    feat: integrate simplebar for custom scrollbar in LibraryExpanded and LibrarySmall components

--- spotify-react/src/components/subcomponents/Library/LibraryExpanded.jsx
+++ spotify-react/src/components/subcomponents/Library/LibraryExpanded.jsx
@@ -3,6 +3,9 @@ import { useGlobalContext } from "../../../GlobalContext";
 import { Link } from "react-router-dom";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
+
 export default function LibraryExpanded({ setIsExpanded }) {
   const [animateClass, setAnimateClass] = useState("collapsed");
   const { libraryItems } = useGlobalContext();
@@ -50,121 +53,123 @@ export default function LibraryExpanded({ setIsExpanded }) {
   }, [searchWord, allItems, selectedType]);
 
   return (
-    <div className={`library-expanded-container ${animateClass}`}>
-      <div className="library-expanded-tools">
-        <button onClick={handleClick}>Expandir</button>
-        <div className="glass-and-input">
-          <FontAwesomeIcon
-            icon={faMagnifyingGlass}
-            size="l"
-            className="library-magnifying-glass"
-            onClick={() => setShowSearch((prev) => !prev)}
-            style={{ cursor: "pointer" }}
-          />
+    <SimpleBar style={{ maxHeight: "100%" }}>
+      <div className={`library-expanded-container ${animateClass}`}>
+        <div className="library-expanded-tools">
+          <button onClick={handleClick}>Expandir</button>
+          <div className="glass-and-input">
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              size="l"
+              className="library-magnifying-glass"
+              onClick={() => setShowSearch((prev) => !prev)}
+              style={{ cursor: "pointer" }}
+            />
 
-          <div
-            className={`library-input-wrapper animated-search ${
-              showSearch ? "expanded" : "collapsed"
-            }`}
-            ref={wrapperRef}
-          >
-            <div className="input-and-results library-input-and-results">
-              <input
-                ref={inputRef}
-                type="text"
-                className="search-input library-search-input"
-                value={searchWord}
-                onChange={(e) => setSearchWord(e.target.value)}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className={"library-buttons-wrapper expanded-buttons-wrapper"}>
-          <button
-            className={selectedType === "playlist" ? "active" : ""}
-            onClick={() => setSelectedType("playlist")}
-          >
-            Playlists
-          </button>
-          <button
-            className={selectedType === "track" ? "active" : ""}
-            onClick={() => setSelectedType("track")}
-          >
-            Músicas
-          </button>
-          <button
-            className={selectedType === "artist" ? "active" : ""}
-            onClick={() => setSelectedType("artist")}
-          >
-            Artistas
-          </button>
-          <button
-            className={selectedType === "album" ? "active" : ""}
-            onClick={() => setSelectedType("album")}
-          >
-            Álbuns
-          </button>
-          <button
-            className={selectedType === "all" ? "active" : ""}
-            onClick={() => setSelectedType("all")}
-          >
-            Tudo
-          </button>
-        </div>
-      </div>
-      <div className="library-expanded-items">
-        {items.map((item) => (
-          <div
-            className="library-expanded-item-rectangle"
-            key={item.id}
-            onMouseEnter={(e) =>
-              (e.currentTarget.style.backgroundColor =
-                "rgba(255, 255, 255, 0.14)")
-            }
-            onMouseLeave={(e) =>
-              (e.currentTarget.style.backgroundColor = "transparent")
-            }
-          >
-            <div className="library-expanded-item-content">
-              <Link to={`/${item.type}/${item.id}`}>
-                <img
-                  alt={item.name}
-                  className={
-                    item.type === "artist"
-                      ? "library-expanded-item-image-artist"
-                      : "library-expanded-item-image"
-                  }
-                  src={
-                    item.type === "artist"
-                      ? item?.images?.[1]?.url
-                      : item?.album?.images?.[1]?.url ||
-                        item?.images?.[1]?.url ||
-                        item?.album?.images[0].url ||
-                        item?.images?.[0].url
-                  }
+            <div
+              className={`library-input-wrapper animated-search ${
+                showSearch ? "expanded" : "collapsed"
+              }`}
+              ref={wrapperRef}
+            >
+              <div className="input-and-results library-input-and-results">
+                <input
+                  ref={inputRef}
+                  type="text"
+                  className="search-input library-search-input"
+                  value={searchWord}
+                  onChange={(e) => setSearchWord(e.target.value)}
                 />
-                <div className="library-expanded-item-info">
-                  <span className="slide-name library-expanded-item-title ">
-                    <Link to={`/${item.type}/${item.id}`}>{item.name}</Link>
-                  </span>
-                  <span className="library-expanded-item-info slide-artist">
-                    {item.type === "artist"
-                      ? item?.artists?.[0]?.name ??
-                        item?.owner?.display_name ??
-                        "Artista"
-                      : item.type === "album"
-                      ? item?.artists?.[0]?.name ?? "Álbum"
-                      : item?.album?.artists?.[0]?.name ??
-                        item?.owner?.display_name ??
-                        "Desconhecido"}
-                  </span>
-                </div>
-              </Link>
+              </div>
             </div>
           </div>
-        ))}
+
+          <div className={"library-buttons-wrapper expanded-buttons-wrapper"}>
+            <button
+              className={selectedType === "playlist" ? "active" : ""}
+              onClick={() => setSelectedType("playlist")}
+            >
+              Playlists
+            </button>
+            <button
+              className={selectedType === "track" ? "active" : ""}
+              onClick={() => setSelectedType("track")}
+            >
+              Músicas
+            </button>
+            <button
+              className={selectedType === "artist" ? "active" : ""}
+              onClick={() => setSelectedType("artist")}
+            >
+              Artistas
+            </button>
+            <button
+              className={selectedType === "album" ? "active" : ""}
+              onClick={() => setSelectedType("album")}
+            >
+              Álbuns
+            </button>
+            <button
+              className={selectedType === "all" ? "active" : ""}
+              onClick={() => setSelectedType("all")}
+            >
+              Tudo
+            </button>
+          </div>
+        </div>
+        <div className="library-expanded-items">
+          {items.map((item) => (
+            <div
+              className="library-expanded-item-rectangle"
+              key={item.id}
+              onMouseEnter={(e) =>
+                (e.currentTarget.style.backgroundColor =
+                  "rgba(255, 255, 255, 0.14)")
+              }
+              onMouseLeave={(e) =>
+                (e.currentTarget.style.backgroundColor = "transparent")
+              }
+            >
+              <div className="library-expanded-item-content">
+                <Link to={`/${item.type}/${item.id}`}>
+                  <img
+                    alt={item.name}
+                    className={
+                      item.type === "artist"
+                        ? "library-expanded-item-image-artist"
+                        : "library-expanded-item-image"
+                    }
+                    src={
+                      item.type === "artist"
+                        ? item?.images?.[1]?.url
+                        : item?.album?.images?.[1]?.url ||
+                          item?.images?.[1]?.url ||
+                          item?.album?.images[0].url ||
+                          item?.images?.[0].url
+                    }
+                  />
+                  <div className="library-expanded-item-info">
+                    <span className="slide-name library-expanded-item-title ">
+                      <Link to={`/${item.type}/${item.id}`}>{item.name}</Link>
+                    </span>
+                    <span className="library-expanded-item-info slide-artist">
+                      {item.type === "artist"
+                        ? item?.artists?.[0]?.name ??
+                          item?.owner?.display_name ??
+                          "Artista"
+                        : item.type === "album"
+                        ? item?.artists?.[0]?.name ?? "Álbum"
+                        : item?.album?.artists?.[0]?.name ??
+                          item?.owner?.display_name ??
+                          "Desconhecido"}
+                    </span>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </SimpleBar>
   );
 }
--- spotify-react/src/components/subcomponents/Library/LibrarySmall.jsx
+++ spotify-react/src/components/subcomponents/Library/LibrarySmall.jsx
@@ -4,6 +4,8 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
 
 export default function LibrarySmall({ setIsExpanded, data }) {
   function handleClick() {
@@ -11,48 +13,50 @@ export default function LibrarySmall({ setIsExpanded, data }) {
   }
 
   return (
-    <div className="library-small-container">
-      <div
-        style={{ width: "75px", height: "75px" }}
-        onClick={handleClick}
-        className="library-icon"
-      >
-        <FontAwesomeIcon
-          icon={faBookOpen}
-          size="2xl"
-          className="icon-default"
-        />
-        <FontAwesomeIcon
-          icon={faArrowsLeftRightToLine}
-          size="2xl"
-          className="icon-hover"
-        />
-      </div>
-      <div className="library-small-list">
-        {data.map((item, index) => (
-          <Link to={`/${item.type}/${item.id}`} key={index}>
-            <div
-              className={
-                item.type === "artist"
-                  ? "library-small-item-artist"
-                  : "library-small-item"
-              }
-            >
-              <img
-                src={
+    <SimpleBar style={{ maxHeight: "100%" }}>
+      <div className="library-small-container">
+        <div
+          style={{ width: "75px", height: "75px" }}
+          onClick={handleClick}
+          className="library-icon"
+        >
+          <FontAwesomeIcon
+            icon={faBookOpen}
+            size="2xl"
+            className="icon-default"
+          />
+          <FontAwesomeIcon
+            icon={faArrowsLeftRightToLine}
+            size="2xl"
+            className="icon-hover"
+          />
+        </div>
+        <div className="library-small-list">
+          {data.map((item, index) => (
+            <Link to={`/${item.type}/${item.id}`} key={index}>
+              <div
+                className={
                   item.type === "artist"
-                    ? item?.images?.[1]?.url
-                    : item?.album?.images?.[1]?.url ||
-                      item?.images?.[1]?.url ||
-                      item?.album?.images?.[0]?.url ||
-                      item?.images?.[0]?.url
+                    ? "library-small-item-artist"
+                    : "library-small-item"
                 }
-                alt={item.name}
-              />
-            </div>
-          </Link>
-        ))}
+              >
+                <img
+                  src={
+                    item.type === "artist"
+                      ? item?.images?.[1]?.url
+                      : item?.album?.images?.[1]?.url ||
+                        item?.images?.[1]?.url ||
+                        item?.album?.images?.[0]?.url ||
+                        item?.images?.[0]?.url
+                  }
+                  alt={item.name}
+                />
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </SimpleBar>
   );
 }
