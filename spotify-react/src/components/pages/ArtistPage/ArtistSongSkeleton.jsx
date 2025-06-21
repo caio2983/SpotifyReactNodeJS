e2cd0d0ab416c8f905dcac9fe28607149bfffbf2
caio2983 <caioluna2002@gmail.com>
2025-06-21 01:10:20 -0300

    refactor: update layout and simplify structure in ArtistSongSkeleton component

--- spotify-react/src/components/pages/ArtistPage/ArtistSongSkeleton.jsx
+++ spotify-react/src/components/pages/ArtistPage/ArtistSongSkeleton.jsx
@@ -4,49 +4,33 @@ import Box from "@mui/material/Box";
 
 export default function ArtistSongSkeleton() {
   return (
-    <div className="song-card" style={{ gridTemplateColumns: "8fr 2fr" }}>
+    <div
+      className="song-card"
+      style={{ gridTemplateColumns: "auto 1fr auto", display: "grid" }}
+    >
       <div className="song-index-album">
         <div className="song-index-number">
-          <div
-            className="song-artist-index-wrapper"
-            style={{ display: "flex", alignContent: "center" }}
-          >
-            <span
-              className="song-index"
-              style={{
-                marginBottom: "0",
-                alignItems: "center",
-              }}
-            >
-              <Skeleton
-                variant="rectangular"
-                sx={{
-                  bgcolor: "#888888",
-                }}
-                height={64}
-                width={64}
-              ></Skeleton>
-            </span>
-          </div>
+          <Skeleton
+            variant="rectangular"
+            sx={{
+              bgcolor: "#888888",
+            }}
+            height={72}
+            width={72}
+          ></Skeleton>
         </div>
 
-        <div className="song-image-wrapper"></div>
-        <div className="name-artist-wrapper" style={{ width: "100%" }}>
-          <span className="song-name">
-            {" "}
-            <Skeleton
-              variant="text"
-              sx={{
-                bgcolor: "#888888",
-                width: "100%",
-                height: "15px",
-              }}
-            ></Skeleton>
-          </span>
-        </div>
+        <div className="gap-fill"></div>
+        <div
+          className="name-artist-wrapper"
+          style={{ width: "100%", display: "flex", alignContent: "center" }}
+        ></div>
       </div>
 
-      <span className="song-duration">
+      <span
+        className="song-duration"
+        style={{ display: "flex", alignItems: "center" }}
+      >
         {" "}
         <Skeleton
           variant="text"
